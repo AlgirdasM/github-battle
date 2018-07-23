09a25e75dbee5788396d86a8b56e9c4b5970fd8e
AlgirdasM <algirdaslt@gmail.com>
2018-07-23 14:56:57 +0300

    feat: add Battle component and route

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Popular from './Popular';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './Home';
+import Battle from './Battle';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
 			<div className="container">
 				<Nav />
 				<Route exact path='/' component={Home} />
+				<Route path='/battle' component={Battle} />
 				<Route path='/popular' component={Popular} />
 			</div>
 		</BrowserRouter>
