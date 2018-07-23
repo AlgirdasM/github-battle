54f406def740843800b82545fa4b2b728911c31e
AlgirdasM <algirdaslt@gmail.com>
2018-07-23 14:42:55 +0300

    feat: add navigation with styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Popular from './Popular'
+import Popular from './Popular';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Nav from './Nav';
 
 class App extends Component {
   render() {
     return (
     	<BrowserRouter>
 			<div className="container">
+				<Nav />
 				<Route path='/popular' component={Popular} />
 			</div>
 		</BrowserRouter>
