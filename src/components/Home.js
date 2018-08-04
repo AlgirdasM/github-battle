2a6c4cbf1589ebd3f1e715708dd245722cd152a6
AlgirdasM <algirdaslt@gmail.com>
2018-08-04 17:23:44 +0300

    feat: add image to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MainImage from './../img/frida-bredesen-317281-unsplash.jpg';
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
@@ -6,6 +7,7 @@ class Home extends Component {
 		return (
 			<div className='home-container'>
 				<h1>Github Battle: Battle your friends... and stuff.</h1>
+				<img className="mainImage shadow" src={MainImage} alt="by Frida Bredesen on Unsplash" />
 				<Link className='button' to='/battle'>
 					Battle
 				</Link>
